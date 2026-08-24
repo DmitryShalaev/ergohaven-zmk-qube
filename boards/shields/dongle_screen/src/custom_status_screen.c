6bfaebec89e88279461b61f786e67009e3b86fbb
Dmitry <dmitryshalaev1901@gmail.com>
2026-08-25 01:19:46 +0300

    Place modifier chips below layer label

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -79,12 +79,18 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
     zmk_widget_layer_status_init(&layer_status_widget, screen_main);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, -18);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
     zmk_widget_mod_status_init(&mod_widget, screen_main);
-    lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_CENTER, 0, 34);
+#if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
+    lv_obj_align_to(zmk_widget_mod_status_obj(&mod_widget),
+                    zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_OUT_BOTTOM_MID, 0,
+                    6);
+#else
+    lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_CENTER, 0, 18);
+#endif
 #endif
 
     k_timer_start(&screen_main_timer, K_SECONDS(2), K_NO_WAIT);
